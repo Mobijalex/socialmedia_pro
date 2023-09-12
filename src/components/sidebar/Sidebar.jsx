0282d42035b1bb2f2006f20172f22ee5e6a0ae52
mobijalex <mobijalex6@gmail.com>
2023-09-12 18:00:37 +0530

    34:24

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
